c04552952979e0fb0b26e8856ef0db18c12c0f35
Azazagich <qwertyu2289@ukr.net>
2024-10-04 11:26:38 +0300

    add: class DemoTaskManagerApplication (entry point)

---
+++ src/main/java/org/example/demotaskmanager/DemoTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.demotaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTaskManagerApplication.class, args);
+	}
+
+}
