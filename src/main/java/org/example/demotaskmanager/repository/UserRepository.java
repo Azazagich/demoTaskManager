6e87796fd87d2fda1f81278be179f29172f2be46
Azazagich <qwertyu2289@ukr.net>
2024-10-04 11:24:36 +0300

    add: method updateToNullUsersByRoleId with JPQL realization

--- src/main/java/org/example/demotaskmanager/repository/UserRepository.java
+++ src/main/java/org/example/demotaskmanager/repository/UserRepository.java
@@ -2,8 +2,17 @@ package org.example.demotaskmanager.repository;
 
 import org.example.demotaskmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User user SET user.role = null WHERE user.role.id = :id")
+    void updateToNullUsersByRoleId(@Param("id") Long id);
 }
